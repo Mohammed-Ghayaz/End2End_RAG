736b8f3a99b8cbc9ee2988e6589c6e3c6ef78fac
Z Mohammed Ghayaz <129831108+Mohammed-Ghayaz@users.noreply.github.com>
2025-11-30 13:00:01 +0000

    Debugging done

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ load_dotenv()
 def test_document_ingestion_and_rag():
     try:
         test_files = [
-            "/Users/yashpatil/Developer/AI/YT/Sunny/LLMOps_series/data/NIPS-2017-attention-is-all-you-need-Paper.pdf",
+            "/workspaces/End2End_RAG/data/Way of All Earth.txt",
         ]
 
         uploaded_files = []
